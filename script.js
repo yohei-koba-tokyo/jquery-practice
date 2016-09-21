da89dc6d0adf9004fbe1c729d065ca148123bf98
Tomoya Yoshioka <tmy.ysok@gmail.com>
2016-09-22 03:34:45 +0900

    Remove unnecessary description

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const API_KEY = '';
 const ABS_TMP_DIFF = 273;
 
 $(function() {
-  var d = new $.Deferred();
   var formSpinner = $('#form-spinner');
   var formError = $('#form-error');
   var formResult = $('#form-result');
