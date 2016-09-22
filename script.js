54cc71799e79d47de1394dfcc781bde25516d4b4
Tomoya Yoshioka <tmy.ysok@gmail.com>
2016-09-23 03:10:37 +0900

    Remove done() and fail()

--- script.js
+++ script.js
@@ -10,26 +10,7 @@ $(function() {
     var cityName = $(this).find('#weather-form-city').prop('value');
     var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
     requestUrl += cityName + '&APPID=' + API_KEY;
-    $.ajax(requestUrl)
-      .done(function(data) {
-        if (data.cod == 200) {
-          formSpinner.css('display', 'none');
-          formError.css('display', 'none');
-          $('#result-city-name').text(data.name);
-          $('#result-temp').text(Math.round(data.main.temp - ABS_TMP_DIFF));
-          $('#result-weather').text(data.weather[0].main);
-          $('#result-datetime').text(Date(data.dt));
-          formResult.css('display', 'block');
-        } else {
-          formSpinner.css('display', 'none');
-          formError.css('display', 'block');
-          formResult.css('display', 'none');
-        }
-      })
-      .fail(function() {
-        formSpinner.css('display', 'none');
-        alert('Something wrong occured.');
-      });
+    $.ajax(requestUrl);
     e.preventDefault();
   });
 });
