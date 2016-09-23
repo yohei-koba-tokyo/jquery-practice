e5b9c277d5e97d2144ec3d4e4bd9c5978c6ef556
Tomoya Yoshioka <tmy.ysok@gmail.com>
2016-09-23 09:29:58 +0900

    Remove on()

--- script.js
+++ script.js
@@ -5,12 +5,4 @@ $(function() {
   var formSpinner = $('#form-spinner');
   var formError = $('#form-error');
   var formResult = $('#form-result');
-  $('#weather-form').on('submit', function(e) {
-    formSpinner.css('display', 'inline');
-    var cityName = $(this).find('#weather-form-city').prop('value');
-    var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
-    requestUrl += cityName + '&APPID=' + API_KEY;
-    $.ajax(requestUrl);
-    e.preventDefault();
-  });
 });
