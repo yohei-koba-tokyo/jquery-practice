117405847545100b7a191dc2790f990f84e6d030
kyohei0423 <kyo199333@gmail.com>
2016-10-05 18:47:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const API_KEY = '';
-const ABS_TMP_DIFF = 273;
 
-$(function() {
-  var formSpinner = $('#form-spinner');
-  var formError = $('#form-error');
-  var formResult = $('#form-result');
-});
