22628d1613c6f504deb2afaad62683f7ed3145d2
Seo Kyohei <Kyohei@Seo.local>
2016-11-23 22:56:19 +0900

    クローンするために不要な記述を消した

--- memo_app/app/controllers/todos_controller.rb
+++ memo_app/app/controllers/todos_controller.rb
@@ -1,23 +1,2 @@
 class TodosController < ApplicationController
-  def index
-    @todo = Todo.new
-    @todos = Todo.order('created_at ASC')
-  end
-
-  def create
-    @todo = Todo.new(todo_params)
-    if @todo.save
-      respond_to do |format|
-        format.html { redierct_to :root }
-        format.json { render json: @todo}
-      end
-    else
-      render :index, alert: 'Todoを入力してください'
-    end
-  end
-
-  private
-  def todo_params
-    params.require(:todo).permit(:content)
-  end
 end
